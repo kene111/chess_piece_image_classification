38df3b5fe79b772efcc5b4eff63e12a77717dbe7
kenechi Ojukwu <kenechiojukwu@gmail.com>
2024-11-12 18:34:48 +0100

    added inference and request size info

--- image_class_client/user_interface.py
+++ image_class_client/user_interface.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 import os
+import sys
+import time
 import base64
 import requests
 import tempfile
@@ -34,11 +36,18 @@ if uploaded_image is not None:
             
         b64_string = base64.b64encode(open(path, "rb").read()) #open(path, "rb").read()
         b64_string = str(b64_string, encoding='utf-8')
-        response = requests.post(url= url_api, json={'img_b64':b64_string})
 
+        req_body = {'img_b64':b64_string}
+        stt = time.time()
+        response = requests.post(url= url_api, json=req_body)
+        et = time.time()
+        elapsed_time = et - stt
+        
         if  response.status_code == 200 :
+            request_size = f"{sys.getsizeof(req_body) /(1<<20):,.4f} MB"
             result = response.json()
-            st.success(f"Chess Piece Prediction: {result['system_response']}")
+            st.success(f"Chess Piece Prediction: {result['system_response']['target']}, Confidence Score: {result['system_response']['confidence']} ")
+            st.write(f"Inference Time: {time.strftime('%H:%M:%S', time.gmtime(elapsed_time))}, Size of Request: {request_size}")
         else:
            st.warning("Something seems wrong !")
             
